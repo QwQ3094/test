790101e2456eb587a3b08c0e40e7e6e6f41b7672
QwQ3094 <1094748790@qqqq.com>
2024-08-24 11:44:46 +0800

    list

--- lib/ui.dart
+++ lib/ui.dart
@@ -118,18 +118,38 @@ void showTestDialog(BuildContext context, VoidCallback onConfirm) {
   );
 }
 
-class BusinessScreen extends StatelessWidget {
+class BusinessScreen extends StatefulWidget {
   const BusinessScreen({super.key});
 
   @override
+  State<BusinessScreen> createState() => _BusinessScreenState();
+}
+
+class _BusinessScreenState extends State<BusinessScreen> {
+  bool _isSwitched = false;
+
+  void _toggleSwitch(bool value) {
+    setState(() {
+      _isSwitched = value;
+    });
+  }
+
+  List<Widget> _getData() {
+    List<Widget> list = [];
+    for (var i = 0; i < 20; i++) {
+      list.add(ListTile(
+        leading: Icon(Icons.reply),
+        title: Text('标题$i'),
+        subtitle: Text('这是第$i个ListTile'),
+      ));
+    }
+    return list;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Text('Business Screen'),
-        ],
-      ),
+    return ListView(
+      children: _getData(),
     );
   }
 }
